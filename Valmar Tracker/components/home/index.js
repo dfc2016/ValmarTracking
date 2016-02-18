d803be62b732726d50f99b98f3babbd709557a91
diego corrada <diego.corrada@gmail.com>
2016-02-18 19:38:04 +0000

    Implemented view Ultimas Posiciones
    Changes made by diego corrada (diego.corrada@gmail.com).

--- Valmar Tracker/components/home/index.js
+++ Valmar Tracker/components/home/index.js
@@ -2,7 +2,56 @@
 
 app.home = kendo.observable({
     onShow: function () {},
-    afterShow: function () {}
+    afterShow: function () {},
+    onShowIniSuperVisor: function () {
+        console.log(" onShowIniSuperVisor >>> OK");
+        console.log(" LoggedUser >>> "+loggedUser);
+        $("#userSuperVisor").html(loggedUser);
+    },
+    afterShowIniSuperVisor: function () {
+        console.log(" afterShowIniSuperVisor >>> OK");
+    },    
+    afterShowUltimasPosiciones: function () {
+        console.log(" afterShowUltimasPosiciones >>> OK");
+        $("#mapUltimasPosiciones").kendoMap({
+            center: [-12.1061, -77.0371],
+            zoom: 12,
+            layers: [
+                {
+                    type: "tile",
+                    urlTemplate: "http://#= subdomain #.tile.openstreetmap.org/#= zoom #/#= x #/#= y #.png",
+                    subdomains: ["a", "b", "c"],
+                },
+                {
+                    //Layer for position of location's device
+                    type: "marker",
+                    dataSource: dsTarget,
+                    locationField: "latlng",
+                    titleField: "name",
+                    shape: "pin",
+                },
+            ],
+        });
+        var dsTarget = new kendo.data.DataSource({
+            data: [
+                {
+                    latlng: [-12.1061, -77.0371],
+                    name: "Vendedor:<b>Marcos</b><br>Fecha:<b>03-02-2016</b><br>Hora:<b>15:20:07</b>"
+                },
+                {
+                    latlng: [-12.1010, -77.0599],
+                    name: "Vendedor:<b>Carlos</b><br>Fecha:<b>10-02-2016</b><br>Hora:<b>10:04:00</b>"
+                },
+                {
+                    latlng: [-12.0710, -77.0799],
+                    name: "Vendedor:<b>Juan</b><br>Fecha:<b>10-02-2016</b><br>Hora:<b>10:04:00</b>"
+                },
+            ]
+        });
+        var map = $("#mapUltimasPosiciones").data("kendoMap");
+        var layerTarget = map.layers[1];
+        layerTarget.setDataSource(dsTarget);
+    },
 });
 
 // START_CUSTOM_CODE_home
@@ -11,22 +60,24 @@ app.home = kendo.observable({
 // END_CUSTOM_CODE_home
 (function (parent) {
     var dataProvider = app.data.valmarTracker;
+    
     var homeModel = kendo.observable({
         fields: {
             selectedRol: "Vendedor",
             username: "",
             isVisible: false,
         },
-        
+
         empezar: function () {
-            console.log(" username >>> " + homeModel.fields.username);
-            console.log(" selectedRol >>> " + homeModel.fields.selectedRol);
+            // console.log(" username >>> " + homeModel.fields.username);
+            // console.log(" selectedRol >>> " + homeModel.fields.selectedRol);
 
             switch (homeModel.fields.username) {
                 case "Marcos":
                 case "Carlos":
                 case "Juan":
-                    console.log(" username >>> OK " + homeModel.fields.username);
+                    // console.log(" username >>> OK " + homeModel.fields.username);
+                    loggedUser = homeModel.fields.username;
                     switch (homeModel.fields.selectedRol) {
                         case "Vendedor":
                             console.log(" Rol >>> Vendedor ");
@@ -43,11 +94,42 @@ app.home = kendo.observable({
                 default:
                     console.log(" username >>> **KO** " + homeModel.fields.username);
                     this.set("homeModel.fields.isVisible", true);
-                    $("#UsrNoRegistrado").html("Usuario: "+homeModel.fields.username+" no registrado");
+                    $("#UsrNoRegistrado").html("Usuario: " + homeModel.fields.username + " no registrado");
             }
+        },
+
+        limpiar: function () {
+        	this.set("homeModel.fields.isVisible", false);
+        	this.set("homeModel.fields.username", "");
         },
     });
     parent.set('homeModel', homeModel);
+    
+    var inicioSupervisorModel = kendo.observable({
+        fields: {
+            loggedUser: loggedUser,
+        },
+        
+        ultimaPosColaboradores: function (e) {
+            console.log("click >>> ultimaPosColaboradores");
+            app.mobileApp.navigate('#components/home/viewUltimasPosiciones.html');
+        },
+        
+        terminar: function (e) {
+            console.log("inicioSupervisorModel >>> click >>> terminar");
+            app.mobileApp.navigate('#components/home/view.html');
+        },
+        
+    });
+    parent.set('inicioSupervisorModel', inicioSupervisorModel);
+    
+    var viewUltimasPosiciones = kendo.observable({
+        terminar: function (e) {
+            console.log("viewUltimasPosiciones >>> click >>> terminar");
+            app.mobileApp.navigate('#components/home/view.html');
+        },        
+    });
+    parent.set('viewUltimasPosiciones', viewUltimasPosiciones);
 
     var listTrackersViewModel = kendo.observable({
         onShow: function (e) {
@@ -62,5 +144,5 @@ app.home = kendo.observable({
 
 // START_CUSTOM_CODE_homeModel
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
-
+var loggedUser = "";
 // END_CUSTOM_CODE_homeModel
